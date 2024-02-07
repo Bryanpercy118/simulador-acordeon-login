da72251f887fd1d062a7c91a9ac0be830e1278eb
Miguel Daza - Prosolvering <madazabran@unicesar.edu.co>
2024-02-06 20:47:57 -0500

    commit

--- data/ui.js
+++ data/ui.js
@@ -1,4 +1,5 @@
-// Seleccionamos el botón de menú y el menú en el DOM
+
+// Seleccionamos el botón de menú
 const menuToggle = document.getElementById('menu-toggle');
 const menu = document.querySelector('.menu');
 
@@ -10,21 +11,23 @@ menuToggle.addEventListener('click', function() {
 
 
 
+// Contenido de la acordeon
+$("#composiciones .contenido").niceScroll({cursorcolor:"#00F",autohidemode:'hover'})
 
-        $("#composiciones .contenido").niceScroll({cursorcolor:"#00F",autohidemode:'hover'})
+// Cargar el sonido del acordeon
+document.body.addEventListener('cargando-sonidos',({detail})=>{
+    console.log({detail})
+    if(detail){
+        document.getElementById("app-container").classList.add("cargando")
+    }else{
+        document.getElementById("app-container").classList.remove("cargando")
+    }
+},true)
 
-        document.body.addEventListener('cargando-sonidos',({detail})=>{
-            console.log({detail})
-            if(detail){
-                document.getElementById("app-container").classList.add("cargando")
-            }else{
-                document.getElementById("app-container").classList.remove("cargando")
-            }
-        },true)
+let composiciones = [];
 
-        let composiciones = [];
 
-// Define default compositions
+// Lista de canciones
 const defaultCompositions = [
 //   {
 //     nombre: "Cancion 1",
@@ -45,15 +48,18 @@ const defaultCompositions = [
 
 ];
 
+// mostrar las canciones
 function cargarComposiciones() {
   $("#composiciones .contenido").html('');
 
-  // Merge default compositions with local storage compositions
+  // Fusionar composiciones predeterminadas con composiciones de almacenamiento local
   composiciones = [
     ...defaultCompositions,
     ...Object.keys({ ...localStorage }).filter((l) => l.includes("acordeon")).map(key => ({ nombre: key.split('-')[1], cancion: JSON.parse(localStorage.getItem(key)) }))
   ];
 
+  
+  //   Botones de la cancion
   composiciones.forEach((composicion, index) => {
     $("#composiciones .contenido").append($(
       `<div class="composicion">
@@ -71,149 +77,154 @@ function cargarComposiciones() {
   });
 }
 
-// Call cargarComposiciones to display default and local storage compositions
+// cargar las canciones
 cargarComposiciones();
 
 
-        $("body").on("click",".acciones .reproducir",function(){
-            const ele = this
-            let composicion     =   composiciones[ele.dataset.index]
-            let duracion        =   composicion.cancion.sort((a,b)=>a.inicio-b.inicio)
-            duracion            =   duracion[duracion.length-1].inicio+duracion[duracion.length-1].duracion
-            const porcentaje    =   $(ele).parent().parent().find(".progreso .porcentaje")
-            porcentaje.css({'animation-duration':duracion+'ms'}).addClass("animar-porcentaje")
+// Reproducir examen
+$("body").on("click",".acciones .reproducir",function(){
+    const ele = this
+    let composicion     =   composiciones[ele.dataset.index]
+    let duracion        =   composicion.cancion.sort((a,b)=>a.inicio-b.inicio)
+    duracion            =   duracion[duracion.length-1].inicio+duracion[duracion.length-1].duracion
+    const porcentaje    =   $(ele).parent().parent().find(".progreso .porcentaje")
+    porcentaje.css({'animation-duration':duracion+'ms'}).addClass("animar-porcentaje")
 
-            Acordeon.reproducir(composiciones[this.dataset.index].cancion)
-        })
+    Acordeon.reproducir(composiciones[this.dataset.index].cancion)
+})
 
-        $("body").on("click",".acciones .borrar",function(){
-            let composicion =   composiciones[this.dataset.index]
-            localStorage.removeItem(`acordeon-${composicion.nombre}`)
-            cargarComposiciones()
-        })
+// Click en iniciar examen
+$("body").on("click",".acciones .practicar",function(){
+    let composicion =   composiciones[this.dataset.index]
+    let duracion    =   composicion.cancion.sort((a,b)=>a.inicio-b.inicio)
+    duracion        =   duracion[duracion.length-1].inicio+duracion[duracion.length-1].duracion
+    
+    const ele = this
+    const porcentaje    =   $(ele).parent().parent().find(".progreso .porcentaje")
+    
+    
+    // Mensaje informativo antes del examen
+    Swal.fire(
+        `Exámen de ${composicion.nombre}`,
+        `Prepárese para tocar la canción con su teclado.`,
+        "info"
+    ).then(()=>{
+        Acordeon.grabar()
 
-        $("body").on("click",".acciones .practicar",function(){
-            let composicion =   composiciones[this.dataset.index]
-            let duracion    =   composicion.cancion.sort((a,b)=>a.inicio-b.inicio)
-            duracion        =   duracion[duracion.length-1].inicio+duracion[duracion.length-1].duracion
-            
-            const ele = this
-            const porcentaje    =   $(ele).parent().parent().find(".progreso .porcentaje")
-            
-           
+        porcentaje.css({'animation-duration':duracion+'ms'}).addClass("animar-porcentaje")
+
+        // Mostrar puntaje
+        setTimeout(()=>{
+            const cancion = Acordeon.detenerGrabacion()
+            const score = Acordeon.evaluar(composicion.cancion,cancion)
+            porcentaje.removeClass("animar-porcentaje")
             Swal.fire(
-                `Exámen de ${composicion.nombre}`,
-                `Prepárese para tocar la canción con su teclado.`,
-                "info"
-            ).then(()=>{
-                Acordeon.grabar()
-
-                porcentaje.css({'animation-duration':duracion+'ms'}).addClass("animar-porcentaje")
-
-
-
-                setTimeout(()=>{
-                    const cancion = Acordeon.detenerGrabacion()
-                    const score = Acordeon.evaluar(composicion.cancion,cancion)
-                    porcentaje.removeClass("animar-porcentaje")
-                    Swal.fire(
-                        'Puntaje obtenido!',
-                        `${score}%`,
-                        score<=30 ? "warning":"success"
-                    )
-                    
-                    
-                },duracion+100)
-            })
-
+                'Puntaje obtenido!',
+                `${score}%`,
+                score<=30 ? "warning":"success"
+            )
             
             
-        })
+        },duracion+100)
+    })
 
-        document.getElementById("grabar").onclick=function(){
-        
-            if(this.classList.contains("grabar")){
-                Acordeon.grabar()
-                this.classList.remove("grabar")
-                this.classList.add("grabando")
-            }else{
-                this.classList.add("grabar")
-                this.classList.remove("grabando")
-                const cancion = Acordeon.detenerGrabacion()
-                const nombre = prompt("Dale un nombre a tu canción")
-                if(nombre){
-                    localStorage.setItem(`acordeon-${nombre}`,JSON.stringify(cancion))
-                    cargarComposiciones()
-                }
-                // Acordeon.reproducir(cancion).then(()=>{
-                //     alert("Reproduccion terminada")
-                // })
-            }
+})
+
+// Click en borrar examen
+$("body").on("click",".acciones .borrar",function(){
+    let composicion =   composiciones[this.dataset.index]
+    localStorage.removeItem(`acordeon-${composicion.nombre}`)
+    cargarComposiciones()
+})
+
+
+// empezar a grabar
+document.getElementById("grabar").onclick=function(){
+
+    if(this.classList.contains("grabar")){
+        Acordeon.grabar()
+        this.classList.remove("grabar")
+        this.classList.add("grabando")
+    }else{
+        this.classList.add("grabar")
+        this.classList.remove("grabando")
+        const cancion = Acordeon.detenerGrabacion()
+        const nombre = prompt("Dale un nombre a tu canción")
+        if(nombre){
+            localStorage.setItem(`acordeon-${nombre}`,JSON.stringify(cancion))
+            cargarComposiciones()
         }
+        // Acordeon.reproducir(cancion).then(()=>{
+        //     alert("Reproduccion terminada")
+        // })
+    }
+}
 
-        document.getElementById("tono-adg").onclick=function(){
-            document.querySelectorAll("#cambiar-tono .opcion").forEach(e=>{
-                e.classList.remove("seleccionada")
-            })
-            Acordeon.cambiarTono('ADG')
-            this.classList.add('seleccionada')
-        }
 
-        document.getElementById("tono-5l").onclick=function(){
-            document.querySelectorAll("#cambiar-tono .opcion").forEach(e=>{
-                e.classList.remove("seleccionada")
-            })
-            Acordeon.cambiarTono('5L')
-            this.classList.add('seleccionada')
-        }
+// Opciones seleccionadas del menú
+document.getElementById("tono-adg").onclick=function(){
+    document.querySelectorAll("#cambiar-tono .opcion").forEach(e=>{
+        e.classList.remove("seleccionada")
+    })
+    Acordeon.cambiarTono('ADG')
+    this.classList.add('seleccionada')
+}
 
-        document.getElementById("tono-gcf").onclick=function(){
-            document.querySelectorAll("#cambiar-tono .opcion").forEach(e=>{
-                e.classList.remove("seleccionada")
-            })
-            Acordeon.cambiarTono('GCF')
-            this.classList.add('seleccionada')
-        }
+document.getElementById("tono-5l").onclick=function(){
+    document.querySelectorAll("#cambiar-tono .opcion").forEach(e=>{
+        e.classList.remove("seleccionada")
+    })
+    Acordeon.cambiarTono('5L')
+    this.classList.add('seleccionada')
+}
 
-        document.getElementById("mano-derecha").onclick=function(){
-            document.querySelectorAll("#cambiar-mano .opcion").forEach(e=>{
-                e.classList.remove("seleccionada")
-            })
-            Acordeon.tocarConLaDerecha()
-            this.classList.add('seleccionada')
-        }
+document.getElementById("tono-gcf").onclick=function(){
+    document.querySelectorAll("#cambiar-tono .opcion").forEach(e=>{
+        e.classList.remove("seleccionada")
+    })
+    Acordeon.cambiarTono('GCF')
+    this.classList.add('seleccionada')
+}
 
-        document.getElementById("mano-izquierda").onclick=function(){
-            document.querySelectorAll("#cambiar-mano .opcion").forEach(e=>{
-                e.classList.remove("seleccionada")
-            })
-            Acordeon.tocarConLaIzquierda()
-            this.classList.add('seleccionada')
-        }
+document.getElementById("mano-derecha").onclick=function(){
+    document.querySelectorAll("#cambiar-mano .opcion").forEach(e=>{
+        e.classList.remove("seleccionada")
+    })
+    Acordeon.tocarConLaDerecha()
+    this.classList.add('seleccionada')
+}
 
-        document.getElementById("modo-teclado").onclick=function(){
-            document.querySelectorAll("#cambiar-modo .opcion").forEach(e=>{
-                e.classList.remove("seleccionada")
-            })
-            Acordeon.cambiarModo(1)
-            this.classList.add('seleccionada')
-        }
+document.getElementById("mano-izquierda").onclick=function(){
+    document.querySelectorAll("#cambiar-mano .opcion").forEach(e=>{
+        e.classList.remove("seleccionada")
+    })
+    Acordeon.tocarConLaIzquierda()
+    this.classList.add('seleccionada')
+}
 
-        document.getElementById("modo-notas").onclick=function(){
-            document.querySelectorAll("#cambiar-modo .opcion").forEach(e=>{
-                e.classList.remove("seleccionada")
-            })
-            Acordeon.cambiarModo(2)
-            this.classList.add('seleccionada')
-        }
+document.getElementById("modo-teclado").onclick=function(){
+    document.querySelectorAll("#cambiar-modo .opcion").forEach(e=>{
+        e.classList.remove("seleccionada")
+    })
+    Acordeon.cambiarModo(1)
+    this.classList.add('seleccionada')
+}
 
-        document.getElementById("modo-numero").onclick=function(){
-            document.querySelectorAll("#cambiar-modo .opcion").forEach(e=>{
-                e.classList.remove("seleccionada")
-            })
-            Acordeon.cambiarModo(0)
-            this.classList.add('seleccionada')
-        }
+document.getElementById("modo-notas").onclick=function(){
+    document.querySelectorAll("#cambiar-modo .opcion").forEach(e=>{
+        e.classList.remove("seleccionada")
+    })
+    Acordeon.cambiarModo(2)
+    this.classList.add('seleccionada')
+}
 
-        
-        Acordeon.iniciar()
+document.getElementById("modo-numero").onclick=function(){
+    document.querySelectorAll("#cambiar-modo .opcion").forEach(e=>{
+        e.classList.remove("seleccionada")
+    })
+    Acordeon.cambiarModo(0)
+    this.classList.add('seleccionada')
+}
+
+// Iniciar sonido del acordeón
+Acordeon.iniciar()
