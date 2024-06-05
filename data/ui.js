0c54e5d02e76a216c2ad6e79d0f6f8ca0f43e720
Miguel Daza <madazabran@unicesar.edu.co>
2024-06-04 21:54:08 -0500

    correcion del ui

--- data/ui.js
+++ data/ui.js
@@ -76,6 +76,28 @@ $("body").on("click", ".acciones .reproducir", function () {
     Acordeon.reproducir(composicion.cancion);
 });
 
+
+
+//-------------------- Proceso de la evaluación -----------------------
+// Inicializar intentos restantes
+
+
+// Click iniciar examen
+$("body").on("click", ".acciones .practicar", function() {
+    let ele = this;
+    let isTerminar = $(ele).text() === "Terminar"; // Verificar si el botón está en estado "Terminar"
+
+    if (isTerminar) {
+        // Acciones para finalizar la evaluación
+        terminarExamen(ele);
+    } else {
+        // Acciones para iniciar el examen
+        iniciarExamen(ele);
+    }
+});
+
+
+
 // Función para iniciar el examen
 function iniciarExamen(ele) {
     const composicion = composiciones[ele.dataset.index];
@@ -85,41 +107,100 @@ function iniciarExamen(ele) {
 
     let intentosRestantes = parseInt(localStorage.getItem('intentosRestantes')) || 3;
 
+     // Decrementar intentos restantes
+    intentosRestantes--;
+    localStorage.setItem('intentosRestantes', intentosRestantes);
+
     function mostrarMensajeIntento() {
-        if (intentosRestantes > 0) {
+       if (intentosRestantes > 0) {
             const intentoActual = 4 - intentosRestantes;
             const mensajeIntento = intentosRestantes > 1 ? `Te quedan ${intentosRestantes - 1} intentos` : 'Este es tu último intento';
-
             Swal.fire({
-                title: `Intento ${intentoActual}`,
-                text: mensajeIntento,
-                icon: "info",
-                showConfirmButton: true,
-                confirmButtonText: 'Comenzar',
-                allowOutsideClick: false,
-                showClass: { popup: 'animated fadeInDown faster' },
-                hideClass: { popup: 'animated fadeOutUp faster' }
+            title: `Intento ${intentoActual}`,
+            text: mensajeIntento,
+            icon: "info",
+            showConfirmButton: true,
+            confirmButtonText: 'Comenzar',
+            allowOutsideClick: false,
+            showClass: { popup: 'animated fadeInDown faster' },
+            hideClass: { popup: 'animated fadeOutUp faster' }
             }).then((result) => {
-                if (result.isConfirmed) {
-                    Acordeon.grabar();
-                    porcentaje.css({ 'animation-duration': duracionTotal + 'ms' }).addClass("animar-porcentaje");
-                    $(ele).text("Terminar");
-                    iniciarTemporizador(ele, duracionTotal);
-                }
+            if (result.isConfirmed) {
+                Acordeon.grabar();
+                porcentaje.css({ 'animation-duration': duracionTotal + 'ms' }).addClass("animar-porcentaje");
+                $(ele).text("Terminar");
+                iniciarTemporizador(ele, duracionTotal);
+            }
             });
         } else {
             Swal.fire({
-                title: '¡Sin intentos disponibles!',
-                text: 'Debes esperar 5 minutos antes de intentarlo de nuevo.',
-                icon: 'warning',
-                confirmButtonText: 'Aceptar',
-                allowOutsideClick: false
+            title: '¡Sin intentos disponibles!',
+            text: 'Debes esperar 5 minutos antes de intentarlo de nuevo.',
+            icon: 'warning',
+            confirmButtonText: 'Aceptar',
+            allowOutsideClick: false
             });
+            mostrarMensajeNoMasIntentos();
         }
+        
     }
 
     mostrarMensajeIntento();
 }
+
+// Función para mostrar mensaje de no más intentos
+function mostrarMensajeNoMasIntentos() {
+    Swal.fire({
+      title: '¡Sin intentos disponibles!',
+      text: 'Debes esperar 5 minutos antes de intentarlo de nuevo.',
+      icon: 'warning',
+      confirmButtonText: 'Aceptar',
+      allowOutsideClick: false
+    }).then(() => {
+      // Bloquear el botón
+      $(".acciones .practicar").prop("disabled", true).addClass("disabled");
+  
+      // Iniciar el cronómetro de 5 minutos
+      let tiempoRestante = 300; // 5 minutos en segundos
+      const cronometroInterval = setInterval(() => {
+        const minutos = Math.floor(tiempoRestante / 60);
+        const segundos = tiempoRestante % 60;
+        const cronometroElement = document.getElementById("cronometro");
+        cronometroElement.textContent = `${minutos.toString().padStart(2, '0')}:${segundos.toString().padStart(2, '0')}`;
+  
+        tiempoRestante--;
+  
+        if (tiempoRestante < 0) {
+          clearInterval(cronometroInterval);
+          $(".acciones .practicar").prop("disabled", false).removeClass("disabled");
+          cronometroElement.textContent = "00:00";
+          // Actualizar la página
+          setTimeout(() => {
+            location.reload();
+          }, 1000);
+        }
+      }, 1000);
+  
+      // Mostrar el cronómetro en la interfaz
+      Swal.fire({
+        html: `
+          <div id="titulo">Esperando 5 minutos para volver a intentarlo...</div>
+            <div class="cronometro-container">
+              <div class="cronometro-box">
+                <div id="cronometro">05:00</div>
+              </div>
+            </div>
+          `,
+        showConfirmButton: false,
+        allowOutsideClick: false,
+        customClass: {
+          popup: 'custom-swal-message'
+        }
+      });
+    });
+  }
+  
+  
 
 // Función para iniciar el temporizador y mostrar el resultado del examen
 function iniciarTemporizador(ele, duracion) {
